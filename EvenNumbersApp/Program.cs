58bd6e98dc47d764710a7f1d90335c63ab5ddb6f
DeimosCreator <andrej.deimos@gmail.com>
2025-11-24 18:02:58 +0300

    1.1. Текущий контроль. Введение в алгоритмы и базовые структуры данных

---
+++ EvenNumbersApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EvenNumbersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создайте массив для 6 чисел
+            int[] numbers = new int[6];
+            // Запросите ввод чисел
+            Console.WriteLine("Введите 6 чисел:");
+            for (int i = 0; i < 6; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            // Проверьте и выведите чётные числа
+            Console.WriteLine("Чётные числа:");
+            for (int i = 0; i < 6; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.WriteLine($"Чётное: {numbers[i]}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
