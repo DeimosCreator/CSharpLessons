10d6ef8ebf12ab8cedcc41b249392e685b3b6efa
DeimosCreator <andrej.deimos@gmail.com>
2025-11-26 20:06:11 +0300

    1.3. Текущий контроль. Массивы и их применение

---
+++ EvenCountApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер массива: ");
+        int size;
+        if (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+        {
+            Console.WriteLine("Введите положительное число!");
+            return;
+        }
+
+        int[] A = new int[size];
+        int evenCount = 0;
+
+        Console.WriteLine("Введите положительные числа массива:\n");
+        for (int i = 0; i < size; i++)
+        {
+            if (!int.TryParse(Console.ReadLine(), out A[i]) || A[i] < 0)
+            {
+                Console.WriteLine("Введите положительное число!");
+                return;
+            }
+            if (A[i] % 2 == 0) evenCount++;
+        }
+
+        Console.WriteLine($"В массиве {evenCount} чётных чисел.");
+    }
+}
