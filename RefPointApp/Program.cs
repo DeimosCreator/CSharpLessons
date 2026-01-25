5fb97fa111dcc9aadde2315a2c4d47dd13c6db98
DeimosCreator <andrej.deimos@gmail.com>
2026-01-25 22:10:03 +0300

    2.3. Текущий контроль. Модификаторы параметров и работа памяти

---
+++ RefPointApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RefPointApp
+{
+    struct Point
+    {
+        public int x, y;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point { x = 1, y = 2 };
+
+            Console.WriteLine($"Исходные координаты: x = {p.x}, y = {p.y}");
+
+            MoveByValue(p, 5, 5);
+            Console.WriteLine($"После MoveByValue: x = {p.x}, y = {p.y}"); // не изменится
+
+            MoveByRef(ref p, 5, 5);
+            Console.WriteLine($"После MoveByRef: x = {p.x}, y = {p.y}");   // изменится
+        }
+
+        static void MoveByValue(Point p, int dx, int dy)
+        {
+            p.x += dx;
+            p.y += dy;
+        }
+
+        static void MoveByRef(ref Point p, int dx, int dy)
+        {
+            p.x += dx;
+            p.y += dy;
+        }
+    }
+}
