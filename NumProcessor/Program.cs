1e1fb2a8702ffc670905a10a4055d37071ee0c7d
DeimosCreator <andrej.deimos@gmail.com>
2026-01-26 18:11:52 +0300

    2.3. Практическая работа. Модификаторы параметров и работа памяти

---
+++ NumProcessor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumProcessor
+{
+    public class NumberProcessor
+    {
+        private double _result;
+
+        public double Result
+        {
+            get => _result; set => _result = value;
+        }
+
+        public double CalculateSquare(in double number) => number * number;
+
+        public void DivideNumbers(double dividend, double divisor, out double quotient)
+        {
+            quotient = divisor != 0 ? dividend / divisor : double.NaN;
+        }
+
+
+        public void AccumulateResult(ref double value)
+        {
+            _result += value;
+            value = _result;
+        }
+
+        public NumberProcessor(double initialResult)
+        {
+            Result = initialResult;
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
